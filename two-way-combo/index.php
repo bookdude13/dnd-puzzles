9c384543d416ef296082925d41e683d4dafbb565
Ryan Fredlund <rfredlund13@gmail.com>
2020-11-01 16:42:39 -0600

    Copying over assets. Moving styles to separate css file

--- two-way-combo/index.php
+++ two-way-combo/index.php
@@ -1,3 +1,10 @@
-<?php
-
-phpinfo();
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Room A</title>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <h1>here</h1>
+</body>
+</html>
