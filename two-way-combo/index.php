9f1617b04f838dedf2e6d456bd1ef3bd536150c5
Ryan Fredlund <rfredlund13@gmail.com>
2020-11-07 07:52:12 -0600

    Adding left and right buttons. Adding jquery and script file

--- two-way-combo/index.php
+++ two-way-combo/index.php
@@ -9,6 +9,9 @@
     <!-- Skeleton styling -->
     <link rel="stylesheet" href="styles/normalize.css">
     <link rel="stylesheet" href="styles/skeleton.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./src/js/main.js"></script>
 </head>
 <body>
     <div class="container">
--- two-way-combo/src/models/GemWheel.php
+++ two-way-combo/src/models/GemWheel.php
@@ -17,11 +17,32 @@ class GemWheel implements iRenderable
         $this->_category = $this->_categories[0];
     }
 
+    private function _prev_category( string $category ) {
+        $index = array_search( $category, $this->_categories, true );
+        $num_categories = count( $this->_categories );
+        $prev_index = ($index + $num_categories - 1) % $num_categories;
+        return $this->_categories[$prev_index];
+    }
+
+    private function _next_category( string $category ) {
+        $index = array_search( $category, $this->_categories, true );
+        $num_categories = count( $this->_categories );
+        $next_index = ($index + 1) % $num_categories;
+        return $this->_categories[$next_index];
+    }
+
     public function get_html(): string {
         $src = "./assets/wheelstones/wheel$this->_category.png";
+
         $html  = '  <div class="three columns">';
         $html .= "      <img class=\"gem-wheel center\" id=\"$this->_id\" src=\"$src\">";
+        $html .= "      <br>";
+        $html .= "      <div class=\"center\">";
+        $html .= "          <button class=\"btn-rotate-left\">&lt;--</button>";
+        $html .= "          <button class=\"btn-rotate-right\">--&gt;</button>";
+        $html .= "      </div>";
         $html .= '  </div>';
+
         return $html;
     }
     
